5385677fb544043b847b9eebe1f8746f0368fe2c
SarahFM4 <fmonteiro.sarah@gmail.com>
2025-10-13 17:33:34 -0300

    Create Usuario.java

---
+++ atividade2/Usuario.java
@@ -0,0 +1,23 @@
+public class Usuario extends Pessoa {
+    private Integer matricula;
+
+    public Usuario(String nome, String email, Integer matricula) {
+        super(nome, email);
+        this.matricula = matricula;
+    }
+
+    @Override
+    public void exibirInfo() {
+        System.out.println("Nome: " + getNome());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Matrícula: " + matricula);
+    }
+
+    public Integer getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(Integer matricula) {
+        this.matricula = matricula;
+    }
+}
